8557d948fc2c0c3a270206b4aeeac333418c4671
Higashi Kakazu <e15002@std.it-college.ac.jp>
2015-11-09 22:05:58 +0900

    P361まで

---
+++ php1/chap15/client.php
@@ -0,0 +1,11 @@
+<?php
+print "あなたのIPアドレスは:";
+print getenv("REMOTE_ADDR");
+print "<BR>";
+print "あなたのホスト名は:";
+print gethostbyaddr(getenv('REMOTE_ADDR'));
+print "<BR>";
+print "あなたのブラウザは:";
+print getenv('HTTP_USER_AGENT');
+print "<BR>ですね";
+?>
---
+++ php1/chap15/idou.php
@@ -0,0 +1,3 @@
+<?php
+print "<meta http-equiv='refresh' content='5;URL=http://www.softbank.co.jp/'>";
+print "5秒後にソフトバンクのページに移動します。";
