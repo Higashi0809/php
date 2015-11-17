bdfdb85b078b3c97982142a73565619a16ad74c4
Higashi Kakazu <e15002@std.it-college.ac.jp>
2015-11-17 22:06:15 +0900

    chap19まで済み

--- php1/chap19/kantan_select.php
+++ php1/chap19/kantan_select.php
@@ -3,8 +3,7 @@
 $s = mysql_connect('localhost','kakazu','1234') or die('失敗です');
 print '成功しました<br>';
 mysql_select_db('db1');
-mysql_query("insert into tb1 values('K780','エスキュー花子', 25)");
-$re = mysql_query("select * from tb1");
+$re = mysql_query("select * from tbk order by bang");
 while($kekka = mysql_fetch_array($re)){
     print $kekka[0];
     print ':';
@@ -14,3 +13,5 @@ while($kekka = mysql_fetch_array($re)){
     print '<br>';
 }
 mysql_close($s);
+
+    print "<br><a href='kantan.html'>トップメニューに戻ります</a>";
