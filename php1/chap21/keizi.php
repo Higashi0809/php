7debfb61d400dabd29fc291f2cee305483d3c320
Higashi Kakazu <e15002@std.it-college.ac.jp>
2015-12-01 22:04:11 +0900

    12/01

--- php1/chap21/keizi.php
+++ php1/chap21/keizi.php
@@ -1,5 +1,4 @@
 <?php
-
 // データベース情報等の読み込み
 require_once('data/db_info.php');
 
@@ -7,4 +6,97 @@ require_once('data/db_info.php');
 $s = mysql_connect($SERV, $USER, $PASS) or die('失敗しました');
 mysql_select_db($DBNM);
 
-print 'OK!';
+// スレッドグループ番号（gu）を取得し$gu_dに代入
+$gu_d=isset($_GET['gu']) ? $_GET['gu']: null;
+
+if(preg_match('/[^0-9]/', $gu_d)){
+// gu_dに数字以外が含まれていたら処理を中止
+    print <<<eot1
+    不正な値が入力されています<br>
+    <a href="keizi_top.php">ここをクリックしてスレッド一覧に戻って下さい</a>
+eot1;
+}elseif(preg_match('/[0-9]/', $gu_d)){
+    // gu-dに数字以外が含まれていない、正常な値での処理
+    $na_d=isset($_GET["na"])?htmlspecialchars($_GET["na"]):null;
+    $me_d=isset($_GET["me"])?htmlspecialchars($_GET["me"]):null;
+
+    // IPアドレス取得
+    $ip=getenv("REMOTE_ADDR");
+
+    // スレッドグループ番号（gu）に一致するレコードを表示
+    $re=mysql_query("SELECT sure FROM tbj0 WHERE guru=$gu_d");
+    $kekka=mysql_fetch_array($re);
+
+    // スレッド内容の表示文字列$sure_comを作成
+    $sure_com="「".$gu_d." ".$kekka[0]."」";
+
+    // スレッド表示のタイトル等書き出し
+     print <<<eot2
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ＳＱＬカフェ $sure_com スレッド</title>
+</head>
+<body bgcolor='darkgray'>
+<font size='7' color='indigo'>
+$sure_com スレッド!
+</font>
+    <br><br>
+    <font size='5'>
+    $sure_com のメッセージ
+    </font>
+    <br>
+
+eot2;
+
+// 名前（$na_d）が入力されていればtbj1にレコード挿入
+if($na_d<>""){
+    mysql_query("INSERT INTO tbj1 VALUES
+        (0,'$na_d','$me_d',now(),$gu_d,'$ip')");
+}
+
+// 水平線表示
+print "<hr>";
+
+// 日時の順にレスデータを表示
+$re=mysql_query("SELECT * FROM tbj1 WHERE guru=$gu_d ORDER BY niti");
+
+$i=1;
+while($kekka=mysql_fetch_array($re)){
+    print "$i(kekka[0]):<U>$kekka[1]</U>:$kekka[3]<br>";
+    print nl2br($kekka[2]);
+    print "<br><br>";
+    $i++;
+}
+
+// データベース切断
+mysql_close($s);
+
+print <<<eot3
+<hr>
+<font size="5">
+$sure_com にメッセージを書くときにはここにどうぞ
+    </font>
+    <form method="GET" action="keizi.php">
+    名前 <input type="text" NAME="na"><br>
+    メッセージ<br>
+    <textarea name="me" rows="10" cols="70"></textarea>
+    <br>
+    <input type="hidden" name="gu" value=$gu_d>
+    <input type="submit" value="送信">
+    </form>
+    <hr>
+    <a href="keizi_top.php">スレッド一覧に戻る</a>
+    </body>
+    </html>
+eot3;
+
+// Sgu_dに数字以外も、数字も含まれていないときの処理
+
+}else{
+    print "スレッドを選択してください。<br>";
+    print "<a href='keizi_top.php'>ここをクリックしてスレッド一覧に戻ってください</a>";
+}
+
+?>
---
+++ php1/chap21/keizi_search.php
@@ -0,0 +1,44 @@
+<?php
+
+//  データベース情報等の読み込み
+require_once('data/db_info.php');
+
+// データベースへ接続、データベース選択
+$s = mysql_connect($SERV, $USER, $PASS) or die('失敗しました');
+mysql_select_db($DBNM);
+
+//print 'OK!';
+
+// タイトルの表示
+print <<<eot1
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ＳＱＬカフェ 検索のページ</title>
+</head>
+<body bgcolor='orange'>
+<hr>
+<font size='5'>
+(検索結果はこちらに)
+</font>
+    <br>
+eot1;
+
+// 検索文字列を取得してタグを削除
+$se_d=isset($_GET["se"])?htmlspecialchars($_GET["se"]):null;
+
+// 検索文字列（$se_d）にデータがあれば検索処理
+if($se_d<>""){
+    // 検索のSQL文 テーブルtbj1にtbj0を結合
+    $str=<<<eot2
+    select tbj1.bang,tbj1.nama,tbj1.mess,tbj0.sure
+    from tbj1
+    join tbj0
+    ON
+    tbj1.guru=tbj0.guru
+    where tbj1.mess like "%$se_d%"
+    eot2;
+
+
+}
